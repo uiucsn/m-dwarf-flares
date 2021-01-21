30a0cc34ca8e20d13a5e283ba116ffacf6fb4040
dev-ved30 <vedgs2@illinois.edu>
2021-01-21 15:46:05 -0600

    LC file now saves the entire flux data

--- flares_test.py
+++ flares_test.py
@@ -1,7 +1,7 @@
 import astropy
 from astropy.io import ascii
 import lightkurve as lk
-from lightkurve import search_targetpixelfile
+from lightkurve import search_lightcurvefile
 import matplotlib.pyplot as plt
 import os.path
 import pandas as pd
@@ -18,11 +18,10 @@ if os.path.isfile("lc_data/KIC-" + KIC_ID + '.csv'):
     lc = lk.LightCurve(time = df['time'], flux = df['flux'], flux_err = df['flux_err'])
 else:
     print('Downloading light curve data ...')
-    pixelfile = search_targetpixelfile(KIC, quarter=16).download();
-    lc = pixelfile.to_lightcurve(aperture_mask='all');
+    lcf = search_lightcurvefile(KIC).download_all()
+    lc = lcf.SAP_FLUX.stitch()
     lc.to_csv("lc_data/KIC-" + KIC_ID + '.csv')
 
-corrected_lc = lc.remove_nans().flatten()
 
 # Reading flare data file.
 flare_data = astropy.io.ascii.read('flare_data/apjaa8ea2t3_mrt.txt', quotechar="\s")
@@ -30,7 +29,7 @@ flare_data = astropy.io.ascii.read('flare_data/apjaa8ea2t3_mrt.txt', quotechar="
 #Plotting points identified as flares.
 for flare in flare_data:
     if flare['KIC'] == int(KIC_ID):
-        if flare['St-BKJD'] > 1520:
-            plt.plot(corrected_lc.time, corrected_lc.flux, linestyle='-', marker='o')
-            plt.xlim([flare['St-BKJD']-0.01,flare['End-BKJD']+0.01])
-            plt.show()
+        plt.plot(lc.time, lc.flux, linestyle='-', marker='o')
+        plt.axvspan(flare['St-BKJD'], flare['End-BKJD'], color='red', alpha=0.2)
+        plt.xlim([flare['St-BKJD']-0.5,flare['End-BKJD']+0.5])
+        plt.show()
