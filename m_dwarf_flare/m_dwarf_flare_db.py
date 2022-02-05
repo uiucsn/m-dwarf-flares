78d8ddba74af3f2b0a8c9f32436f1d31864dafdb
Ved Shah <vedshah.email@gmail.com>
2022-02-05 05:57:04 -0600

    Added ability to write only local flares to file. Added ability to make plots for the selection

--- m_dwarf_flare/m_dwarf_flare_db.py
+++ m_dwarf_flare/m_dwarf_flare_db.py
@@ -13,6 +13,7 @@ from astropy.coordinates import ICRS
 import astropy_healpix
 from astropy import units as u
 import matplotlib.pyplot as plt
+from m_dwarf_flare.plotting_tools import plotGenricSkyMap
 
 class MDwarfFlareDB:
 
@@ -65,7 +66,7 @@ class MDwarfFlareDB:
 
         return hp_index
 
-    def get_flares_in_skymap_ci(self, skymap_path, confidence_interval, lclib_path, pic_path):
+    def get_flares_in_skymap_ci(self, skymap_path, confidence_interval, lclib_path, to_plot):
 
         skymap = Table.read(skymap_path)
         nside = hp.npix2nside(len(skymap))
@@ -73,14 +74,29 @@ class MDwarfFlareDB:
         mask, high_prob_flare_indices = self.get_confidence_interval_mask(skymap, confidence_interval)
         healpix_indices = self.get_flare_healpix_indices(nside)
 
-        coordinates = []
+        ra = []
+        dec = []
         with open(lclib_path, 'w') as output_file:
             for i in range(len(healpix_indices)):
                 if (healpix_indices[i] in high_prob_flare_indices):
                     for row in self.cur.execute('SELECT flare_object FROM flares WHERE flare_index = {}'.format(i)):
                         flare = pickle.loads(row[0])
                         flare.dump_flare_to_LCLIB(output_file)
+                        if to_plot:
+                            ra.append(flare.coordinates.ra)
+                            dec.append(flare.coordinates.dec)
 
+        if to_plot:
+
+            plotGenricSkyMap(SkyCoord(ra=ra*u.deg, dec=dec*u.deg))
+
+            hp.mollview(np.log10(skymap['PROB']), nest=True, min = -3, max = 0, title="Log Prob for GW event")
+            hp.graticule(coord="E")
+
+            hp.mollview(mask , nest=True, cmap='Greys',title="{}% Confidence interval healpix mask".format(confidence_interval * 100))
+            hp.graticule(coord="E")
+
+            plt.show()
 
 
 def main():
@@ -127,8 +143,8 @@ def gw_event_localized_flares():
                             help='Path to the fits file of the GW event. Should be a single order fits file. Uses the same nside value as fit file for picking the flares using healpix')
         argparser.add_argument('--con_int', type=float, required=True,
                             help='Confidence interval of the area from which the flares are picked. CI should be between 0 and 1 inclusive')
-        argparser.add_argument('--picture_path', type=str, required=False, default=None,
-                            help='Save an image of the CI area mask and the flare objects skymap next to it')
+        argparser.add_argument('--make_plots',  required=False, action='store_true',
+                            help='Construct plots for GW events, the CI area mask and the flare objects skymap')
         args = argparser.parse_args()
 
         if not args.output_file_path.endswith(".TEXT"):
@@ -143,5 +159,5 @@ def gw_event_localized_flares():
     args = parse_args()
 
     flare_db = MDwarfFlareDB(args.db_path)
-    flare_db.get_flares_in_skymap_ci(args.fits_file_path, args.con_int, args.output_file_path, args.picture_path)
+    flare_db.get_flares_in_skymap_ci(args.fits_file_path, args.con_int, args.output_file_path, args.make_plots)
 
